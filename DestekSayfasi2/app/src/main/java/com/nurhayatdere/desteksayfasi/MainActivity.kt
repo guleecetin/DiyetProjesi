fb824a20cfe9aac54f4480feaec2a3aeb7a26ed1
Nurhayat Dere <nrhyt1746@gmail.com>
2024-05-29 22:24:36 +0300

    Destek Sayfası giriş bölümleri oluşturuldu

---
+++ DestekSayfasi2/app/src/main/java/com/nurhayatdere/desteksayfasi/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.nurhayatdere.desteksayfasi
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.nurhayatdere.desteksayfasi.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+    }
+
+    fun diyetisyen(view: View){
+
+    }
+
+
+    fun sporkocu(view: View){
+
+    }
+
+    fun psikoloji(view: View){
+
+    }
+
+    fun ilan(view: View){
+
+    }
+}
